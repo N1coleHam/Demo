384963446cb14df375b4dd8d42f8e805dd52bffe
Nicole Ham <nicole.xi.ham@gmail.com>
2024-11-27 09:39:02 -0800

    Setup

--- demo.java
+++ demo.java
@@ -1,8 +1,6 @@
 public class demo {
     public static void main(String[] args) {
-
-        System.out.println("This is our project. It's very cool");
-        System.out.println("Nicole was here and she made a AMAZING edit");
-        System.out.println("HEllo!");
+        // Try deleting me!
+        System.out.println("This is our project. As you can see, it's very cool");
     }
 }
