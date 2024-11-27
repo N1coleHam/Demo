f68928898277c6c86de82d098313b1e63ce81fec
Connor Sun <connorsun@gmail.com>
2024-11-27 13:08:40 -0800

    delete everything!!!

--- demo.java
+++ demo.java
@@ -1,6 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("This is our project. As you can see, it's very cool");
-        System.out.println("Nicole is so cool!");
+        // haha i deleted your code
     }
 }
