6fdefc8971ae80068c1efa8286471384d386b145
Nicole Ham <nicole.xi.ham@gmail.com>
2024-11-27 14:33:48 -0800

    Setup

--- demo.java
+++ demo.java
@@ -2,5 +2,7 @@ public class demo {
     public static void main(String[] args) {
         // DELETE MEEE
         System.out.println("This is our very cool project");
+
+        // Add something here!
     }
 }
