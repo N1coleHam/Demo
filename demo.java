b81948e8946aa348d5902b6e888cedebda25232d
Nicole Ham <nicole.xi.ham@gmail.com>
2024-11-27 13:07:12 -0800

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main(String[] args) {
-        // Try deleting me!
         System.out.println("This is our project. As you can see, it's very cool");
+        System.out.println("Nicole is so cool!");
     }
 }
