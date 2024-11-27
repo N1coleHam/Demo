3055a3124a0aaf04f1545b24d9cae1789b810652
Nicole Ham <nicole.xi.ham@gmail.com>
2024-11-26 23:21:39 -0800

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,8 +1,8 @@
 public class demo {
     public static void main(String[] args) {
-        // Cool commit
 
         System.out.println("This is our project. It's very cool");
         System.out.println("Nicole was here and she made a AMAZING edit");
+        System.out.println("HEllo!");
     }
 }
