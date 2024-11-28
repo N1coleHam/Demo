6600777ea7cae27a4c27f6d18581c48782fd4a2f
Nicole Ham <nicole.xi.ham@gmail.com>
2024-11-27 16:11:25 -0800

    I made some changes to Demo.java

--- demo.java
+++ demo.java
@@ -1,8 +1,8 @@
 public class demo {
     public static void main(String[] args) {
-        // DELETE MEEE
         System.out.println("This is our very cool project");
 
         // Add something here!
+        System.out.println("WEEEE");
     }
 }
